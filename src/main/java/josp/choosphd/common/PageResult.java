2dcacad59a2735ff489a006a14e43b49b1f75b14
junw <wo1261931780@gmail.com>
2026-06-23 13:16:30 +0800

    chore: remove legacy code for rebuild

--- src/main/java/josp/choosphd/common/PageResult.java
+++
@@ -1,9 +0,0 @@
-package josp.choosphd.common;
-
-import java.util.List;
-
-public record PageResult<T>(List<T> list, long total, long pageNo, long pageSize) {
-    public static <T> PageResult<T> of(List<T> list, long total, long pageNo, long pageSize) {
-        return new PageResult<>(list, total, pageNo, pageSize);
-    }
-}
